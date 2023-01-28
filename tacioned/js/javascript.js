c73489693d52973a479dacc8389ab937d8b74016
tacionlabs <117913794+tacionlabs@users.noreply.github.com>
2023-01-28 22:10:59 +0200

    Deploy

---
+++ tacioned/js/javascript.js
@@ -0,0 +1,40 @@
+function dropMenue() {
+    var x = document.getElementById("slider");
+
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none"
+    }
+}
+function closeMenue() {
+    var x = document.getElementById("slider");
+
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block"
+    }
+}
+function aboutMenue() {
+    var x = document.getElementById("slider");
+
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block"
+    }
+}
+function mapMenue() {
+    var x = document.getElementById("slider");
+
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block"
+    }
+}
